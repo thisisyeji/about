e670dfc0d1ed34595561b34153a0294d5b559824
yjkim0109 <yjkkim0109@gmail.com>
2022-11-22 19:02:08 +0900

    Design: 컴포넌트 스타일링

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,12 +1,30 @@
-import React, { forwardRef } from 'react';
-import Common from './Common';
+import React, { forwardRef, useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const ImgWrap = styled.div`
+	width: 100%;
+	height: 100vh;
+	padding-top: 150px;
+`;
+
+const Contents = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+
+	.img {
+		width: 30vw;
+		padding: 10px;
+
+		img {
+			width: 100%;
+		}
+	}
+`;
 
 const Images = forwardRef((props, ref) => {
-	return (
-		<div ref={ref}>
-			<Common>Images</Common>
-		</div>
-	);
+	return <ImgWrap ref={ref}></ImgWrap>;
 });
 
 export default Images;
