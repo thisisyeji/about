c613d5a010c10519f23226ce399cb22cf41bc4e0
yjkim0109 <yjkkim0109@gmail.com>
2022-11-22 19:03:57 +0900

    Feat: Unsplash api 추가

--- src/components/Images.js
+++ src/components/Images.js
@@ -24,7 +24,48 @@ const Contents = styled.div`
 `;
 
 const Images = forwardRef((props, ref) => {
-	return <ImgWrap ref={ref}></ImgWrap>;
+	const [like, setLike] = useState('dog');
+	const [result, setResult] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const likeSearch = async () => {
+		setIsLoading(true);
+		try {
+			const res = await axios.get(
+				`https://api.unsplash.com/search/photos?page=1&query=${like}&client_id=Ox7dtMpFz2TuEfmOOGIcqZ1QcXhMWo-uedQ3eb-LoUY`
+			);
+			setResult(res.data.results);
+		} catch (err) {
+			console.log(err);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		likeSearch();
+	}, [like]);
+	return (
+		<ImgWrap ref={ref}>
+			<h2>TMI</h2>
+			<p>
+				<strong onClick={() => setLike('dog')}>동물</strong>을 좋아해요.🐶
+			</p>
+			<p>
+				<strong onClick={() => setLike('travel')}>여행</strong>을 좋아해요.✈️
+			</p>
+			<Contents>
+				{isLoading
+					? 'Loading...'
+					: result.map((it, idx) => (
+							<div className='img' key={idx}>
+								<img src={it.urls.regular} alt={it.id} />
+							</div>
+					  ))}
+			</Contents>
+			;
+		</ImgWrap>
+	);
 });
 
 export default Images;
