dfe4b51fe37b0ac784e97bb265006c2f1879a312
yjkim0109 <yjkkim0109@gmail.com>
2022-11-24 12:55:49 +0900

    Feat: Swiper 삭제, Material UI 적용 및 스타일링

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,28 +1,42 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Detail from './Detail';
 
 const Title = styled.h2`
-	font-size: 50px;
-	font-weight: 700;
-	color: #000;
+	color: #222;
 	transform: translateX(-50px);
 	transition: 0.5s;
 `;
 
-const InfoBox = styled.div`
+const Wrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	gap: 50px;
+
+	.content {
+		transform: translateY(100px);
+		transition: 0.5s;
+	}
+`;
+
+const InfoBox = styled.section`
 	width: 100%;
 	height: 100vh;
 	padding: 50px;
-	padding-top: 150px;
+	padding-top: 100px;
 	opacity: 0;
 	transition: 0.5s;
 
 	display: flex;
-	gap: 50px;
+	justify-content: center;
+	align-items: center;
 
 	&.on {
 		opacity: 1;
@@ -30,51 +44,43 @@ const InfoBox = styled.div`
 		${Title} {
 			transform: translateX(0);
 		}
-	}
-`;
 
-const SwiperStyle = styled(Swiper)`
-	position: relative;
-	.swiper-pagination {
-		.swiper-pagination-bullet-active {
-			width: 50px;
-			border-radius: 10px;
-			background: #333;
+		.content {
+			transform: translateY(0);
 		}
 	}
-`;
-
-const Slide = styled(SwiperSlide)`
-	width: 400px;
-	height: 500px;
-	border: 1px solid #000;
 `;
 
 const Project = forwardRef((props, ref) => {
 	return (
 		<InfoBox ref={ref}>
-			<Title>PROJECT</Title>
-			<SwiperStyle
-				modules={[Pagination, Autoplay]}
-				pagination={{ clickable: true }}
-				// 간격
-				spaceBetween={30}
-				// 오토 플레이
-				autoplay={{ delay: 3000, disableOnInteraction: true }}
-				// 순환
-				loop={true}
-				// 한번에 보여질 슬라이드 갯수
-				slidesPerView={1}
-				breakpoints={{
-					764: {
-						slidesPerView: 2,
-					},
-				}}>
-				<Slide>Slide 1</Slide>
-				<Slide>Slide 2</Slide>
-				<Slide>Slide 3</Slide>
-				<Slide>Slide 4</Slide>
-			</SwiperStyle>
+			<Wrapper>
+				<Title>Project</Title>
+				<div className='content'>
+					<Accordion>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls='panel1a-content'
+							id='panel1a-header'>
+							<Typography>팀 프로젝트 - FUN편 log</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+							<Detail />
+						</AccordionDetails>
+					</Accordion>
+					<Accordion>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls='panel2a-content'
+							id='panel2a-header'>
+							<Typography>개인 프로젝트 - 프로젝트1</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+							<Detail />
+						</AccordionDetails>
+					</Accordion>
+				</div>
+			</Wrapper>
 		</InfoBox>
 	);
 });
