f014fce49c0a08dffa6f5888684332cbf7549b58
yjkim0109 <yjkkim0109@gmail.com>
2022-11-24 11:52:20 +0900

    Feat: 레이아웃 및 스타일링

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,84 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
+import { MdMailOutline } from 'react-icons/md';
 
-const ContactWrap = styled.div`
+const Title = styled.h2`
+	color: #fff;
+	font-size: 5vw;
+	font-weight: 900;
+	font-style: italic;
+	text-align: center;
+	text-shadow: -1px 0 #222, 0 1px #222, 1px 0 #222, 0 -1px #222;
+	margin-bottom: 50px;
+	transition: 0.5s;
+`;
+
+const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+
+	&:hover {
+		svg {
+			transform: scale(1.2);
+		}
+		span {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+
+	svg {
+		font-size: 4vw;
+		color: #fff;
+		background: #222;
+		border: 1px solid #555;
+		border-radius: 100px;
+		padding: 15px;
+		margin-bottom: 40px;
+		transition: 0.5s;
+	}
+
+	span {
+		opacity: 0;
+		transform: translateY(-20px);
+		font-size: 3vw;
+		margin-left: 10px;
+		transition: 0.5s;
+	}
+`;
+
+const ContactWrap = styled.section`
 	width: 100%;
-	height: 100vh;
-	background: #efefef;
+	height: 60vh;
 	padding: 50px;
-	padding-top: 150px;
-	box-sizing: border-box;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	&.on {
+		opacity: 1;
+
+		${Title} {
+			transform: scale(1.2);
+		}
+	}
 `;
 
 const Contact = forwardRef((props, ref) => {
-	return <ContactWrap ref={ref}></ContactWrap>;
+	return (
+		<ContactWrap ref={ref}>
+			<Title>CONTACT ME!</Title>
+			<Content>
+				<MdMailOutline />
+				<span>yjkkim0109@gmail.com</span>
+			</Content>
+		</ContactWrap>
+	);
 });
 
 export default Contact;
