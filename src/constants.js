a5d2de7256053dc57220b586a76edf2fcfa5eae9
yjkim0109 <yjkkim0109@gmail.com>
2022-12-23 15:52:21 +0900

    feat : constants 파일 추가

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+export const LIST = ['Intro', 'Projects', 'Contact'];
+
+export const PROJECT_LIST = [
+	{
+		id: 0,
+		title: '팀 프로젝트 - FUN편 log',
+		gitHubUrl: 'https://github.com/Couch-Coders/12th-Fun-Pyeon-log-fe',
+		siteUrl: 'https://majestic-babka-2e99ac.netlify.app/',
+		desc: 'Kakaomap Api를 활용하여 제작한 지도 기반 편의점 리뷰 웹사이트입니다. 실제 위치 기반 서비스로 구글 OAuth 로그인, 지도 검색, 편의점 리스트 필터링, 리뷰 CRUD 등의 기능을 구현하였습니다.',
+		front: 'React, TypeScript, Styled-Components, Redux-toolkit',
+		back: 'spring-boot',
+	},
+	{
+		id: 1,
+		title: '개인 프로젝트 - 트위터 클론 코딩',
+		gitHubUrl: 'https://github.com/thisisyeji/kwitter',
+		siteUrl: 'https://thisisyeji.github.io/kwitter',
+		desc: 'Firebase를 활용하여 제작한 개인 포트폴리오용 트위터 클론 코딩 웹사이트입니다. 로그인 및 실시간 게시글 CRUD 등의 기능을 구현하였습니다.',
+		front: 'React, JavaScript, Styled-Components',
+		back: '',
+	},
+	{
+		id: 2,
+		title: '개인 프로젝트 - React 기업형 웹사이트',
+		gitHubUrl: 'https://github.com/thisisyeji/react_p',
+		siteUrl: 'https://thisisyeji.github.io/react_p',
+		desc: 'Main, About, Youtube, Gallery, Community, Location, Members 페이지로 구성된 React로 제작한 개인 포트폴리오용 기업형 웹사이트입니다.',
+		front: 'React, JavaScript, SCSS',
+		back: '',
+	},
+];
